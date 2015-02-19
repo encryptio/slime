b5d9ab9f3ec6a0fa63541b26e82ba5401fdb3620
Chris Kastorff <encryptio@gmail.com>
2015-02-19 10:36:11 -0800

    lib/store/multi: rebalancing now runs 8x more often

--- lib/store/multi/multi_rebalance.go
+++ lib/store/multi/multi_rebalance.go
@@ -15,13 +15,13 @@ import (
 // TODO: tests for rebalancing (requires mocking/overriding FreeSpace())
 
 var (
-	rebalanceWait = time.Minute * 5
+	rebalanceWait = time.Minute * 3
 
 	errModifiedDuringBalance = errors.New("file modified during balance")
 )
 
 const (
-	rebalanceFileCount     = 10
+	rebalanceFileCount     = 50
 	rebalanceMaxScan       = 500
 	rebalanceMinDifference = 1024 * 1024 * 1024 // 1 GiB
 )
