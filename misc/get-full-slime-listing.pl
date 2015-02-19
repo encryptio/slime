f8b5e11d7cfa5f8e81ff01bb506ae3d6fbecdca4
Chris Kastorff <encryptio@gmail.com>
2015-02-19 09:38:40 -0800

    misc/get-full-slime-listing: added

---
+++ misc/get-full-slime-listing.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+use LWP::Simple;
+use URI::Escape;
+
+my $data_url = shift;
+die "Usage: $0 base_url\n" if @ARGV or not defined $data_url;
+
+my $page_size = 1000;
+my $after = "";
+while ( 1 ) {
+    my $url = $data_url."?mode=list&limit=$page_size&after=".uri_escape($after);
+    my $data = get $url;
+    last if length($data) == 0;
+    print $data;
+    my $last = ($data =~ /(?:^|\n)([^\n]+)$/s)[0];
+    die unless defined $last;
+    $after = $last;
+}
