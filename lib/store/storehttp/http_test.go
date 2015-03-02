cbe334ff3ca672632782c0f8ba57d03e780af2d9
Chris Kastorff <encryptio@gmail.com>
2015-03-01 23:22:27 -0800

    lib/store/storehttp: add test for large request bodies

--- lib/store/storehttp/http_test.go
+++ lib/store/storehttp/http_test.go
@@ -1,6 +1,8 @@
 package storehttp
 
 import (
+	"bytes"
+	"net/http"
 	"net/http/httptest"
 	"os"
 	"testing"
@@ -27,5 +29,27 @@ func TestHTTPCommon(t *testing.T) {
 	if client.UUID() != ds.UUID() {
 		t.Errorf("client UUID %v does not match directory UUID %v",
 			uuid.Fmt(client.UUID()), uuid.Fmt(ds.UUID()))
+	}
+}
+
+func TestHTTPTooBig(t *testing.T) {
+	ds, tmpDir := storedir.MakeTestingDirectory(t)
+	defer os.RemoveAll(tmpDir)
+
+	handler := NewServer(ds)
+
+	data := make([]byte, MaxFileSize+1000)
+
+	w := httptest.NewRecorder()
+	req, err := http.NewRequest("PUT", "/thing", bytes.NewReader(data))
+	if err != nil {
+		t.Fatalf("Couldn't create request: %v", err)
+	}
+
+	handler.ServeHTTP(w, req)
+
+	if w.Code != http.StatusRequestEntityTooLarge {
+		t.Errorf("Wanted response code %v, but got %v",
+			http.StatusRequestEntityTooLarge, w.Code)
 	}
 }
