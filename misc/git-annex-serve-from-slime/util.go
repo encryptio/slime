1a73a308080d1416438ffc97f9a648062812fad9
Chris Kastorff <encryptio@gmail.com>
2015-03-18 20:47:24 -0700

    misc/git-annex-serve-from-slime: less logging

--- misc/git-annex-serve-from-slime/util.go
+++ misc/git-annex-serve-from-slime/util.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"log"
 	"os/exec"
 )
 
 func run(name string, args ...string) ([]byte, error) {
-	log.Printf("Running %v with args %v", name, args)
 	cmd := exec.Command(name, args...)
 	cmd.Dir = *repo
 	return cmd.Output()
