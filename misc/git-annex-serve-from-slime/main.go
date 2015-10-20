67add7b76cab751b00c849cba0b29b8c8da05214
Chris Kastorff <encryptio@gmail.com>
2015-10-19 17:32:03 -0700

    misc/git-annex-serve-from-slime: add request logging

--- misc/git-annex-serve-from-slime/main.go
+++ misc/git-annex-serve-from-slime/main.go
@@ -5,12 +5,15 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"git.encryptio.com/slime/lib/httputil"
 )
 
 var (
-	addr   = flag.String("addr", ":8080", "listening address")
-	repo   = flag.String("repo", ".", "git annex repository to serve from")
-	remote = flag.String("remote", "slime", "remote to use for slime")
+	addr    = flag.String("addr", ":8080", "listening address")
+	repo    = flag.String("repo", ".", "git annex repository to serve from")
+	remote  = flag.String("remote", "slime", "remote to use for slime")
+	loghttp = flag.Bool("loghttp", true, "log http requests")
 )
 
 func main() {
@@ -32,6 +35,13 @@ func main() {
 
 	log.Printf("got base url %v", baseurl)
 
-	log.Fatal(http.ListenAndServe(*addr,
-		http.FileServer(&FileSystem{BaseURL: baseurl, UUID: uuid})))
+	var handler http.Handler = http.FileServer(&FileSystem{
+		BaseURL: baseurl,
+		UUID:    uuid,
+	})
+	if *loghttp {
+		handler = httputil.LogHTTPRequests(handler)
+	}
+
+	log.Fatal(http.ListenAndServe(*addr, handler))
 }
